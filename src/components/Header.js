2b9570592905be42e3e2a75a1bdceb1e6ee4a518
Daria Andreeva <dd1556@mail.ru>
2022-12-26 23:38:49 +0300

    add favorote section to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,13 @@ function Header(props) {
                     <p className="opacity-5">Магазин лучших кроссовок</p>
                 </div>
             </div>
-            <ul className="d-flex">
-                <li className="mr-30">
+            <ul className="d-flex align-center">
+                <li className="d-flex mr-30">
                     <img onClick={props.onClickCart} className ="cu-p" width={18} height={18} src="./img/cart.svg" alt="" />
                     <span>1205 руб.</span>
+                </li>
+                <li>
+                    <img src="./img/card-unliked.svg" alt="Favorites" />
                 </li>
                 <li>
                     <img width={18} height={18} src="./img/user.svg" alt="" />
